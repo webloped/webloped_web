8c69cdc3f8b91c85a5a220195e5a360fbbafcf56
Lakshay Sharma <59906345+la2sharma@users.noreply.github.com>
2024-07-25 10:26:56 -0400

    Added templates page

--- templates/010 little-squirrel-v1.0.0/README.md
+++
@@ -1,3 +0,0 @@
-## Little squirrel theme by ThemeWagon team.
----
-Thank you for using Little squirrel See the "public" folder, you will find everything ready to use there. If you want to use the gulp based workflow, cd to this directory in your terminal and run this command: npm i && gulp
--- templates/056 ensurance-v1.0.0/src/js/theme.js
+++
@@ -1,9 +0,0 @@
-import { docReady } from "./utils";
-import navbarInit from "./navbar-darken-on-scroll";
-import detectorInit from "./detector";
-
-/* -------------------------------------------------------------------------- */
-/*                            Theme Initialization                            */
-/* -------------------------------------------------------------------------- */
-docReady(navbarInit);
-docReady(detectorInit);
